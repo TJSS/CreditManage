5230b41b17d3deffcbffef4b510e1d7f6f3fd3a2
catoldcui <catoldcui@gmail.com>
2014-09-04 11:51:07 +0800

    添加shh2一些

--- CreditManage/src/main/java/com/pingancar/creditmanage/action/XXAction.java
+++ CreditManage/src/main/java/com/pingancar/creditmanage/action/XXAction.java
@@ -1,7 +1,19 @@
 package com.pingancar.creditmanage.action;
 
+import com.opensymphony.xwork2.ActionSupport;
+import com.pingancar.creditmanage.service.XXService;
+
 /**
  * Created by Administrator on 2014/9/3 0003.
  */
-public interface XXAction {
+public class XXAction extends ActionSupport{
+    private XXService xxService;
+
+    public XXService getXxService() {
+        return xxService;
+    }
+
+    public void setXxService(XXService xxService) {
+        this.xxService = xxService;
+    }
 }
