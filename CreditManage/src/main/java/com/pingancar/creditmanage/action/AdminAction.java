1e26ae83afb4f592a55a4b8c77723645e64f099d
catoldcui <catoldcui@gmail.com>
2014-09-13 10:58:34 +0800

    adminaction

--- CreditManage/src/main/java/com/pingancar/creditmanage/action/AdminAction.java
+++ CreditManage/src/main/java/com/pingancar/creditmanage/action/AdminAction.java
@@ -1,8 +1,10 @@
 package com.pingancar.creditmanage.action;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.pingancar.creditmanage.pojo.AdminPojo;
 import com.pingancar.creditmanage.service.AdminService;
+import com.pingancar.creditmanage.util.myenum.UserType;
 
 /**
  * Created by Administrator on 2014/9/9 0009.
@@ -14,11 +16,18 @@ public class AdminAction extends ActionSupport {
     public String login(){
         boolean result = adminService.login(adminPojo);
         if(result){
+            ActionContext.getContext().getSession().put("user", adminPojo);
+            ActionContext.getContext().getSession().put("type", UserType.Admin);
             return SUCCESS;
         } else {
             addActionError("登录失败");
             return ERROR;
         }
+    }
+
+    public String logout(){
+        ActionContext.getContext().getSession().put("type", UserType.Reader);
+        return SUCCESS;
     }
 
     public AdminService getAdminService() {
